20de86de49922641b94bb42e4a7981f96faed08f
Dinda Lusita <dindafristiani@gmail.com>
2024-07-25 12:46:26 +0700

    hapus file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,6 @@
 
   <!-- Favicons -->
   <link href="{{asset ('assets/img/favicon.png')}}" rel="icon">
-  <link href="{{asset ('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -20,9 +19,6 @@
   <!-- Vendor CSS Files -->
   <link href="{{asset ('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{asset ('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{asset ('assets/vendor/aos/aos.css" rel="stylesheet') }}">
-  <link href="{{asset ('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{asset ('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Main CSS File -->
   <link href="{{asset('assets/css/main.css')}}" rel="stylesheet">
@@ -83,10 +79,6 @@
 
   <!-- Vendor JS Files -->
   <script src="{{asset ('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset ('assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset ('assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{asset ('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset ('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
 
   <!-- Main JS File -->
   <script src="{{asset('assets/js/main.js')}}"></script>
